4dc951bc271f9dea682fd9cd5bc14667744e77da
Rohitmeena8923 <157653731+Rohitmeena8923@users.noreply.github.com>
2025-07-11 20:26:44 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,9 +1,18 @@
+import threading
 from pyrogram import Client
 from config import Config
+from server import app  # Flask app import
 
+# Start Flask server in background
+def run_flask():
+    app.run(host="0.0.0.0", port=10000)
+
+threading.Thread(target=run_flask).start()
+
+# Pyrogram bot start
 plugins = dict(root="plugins")
 
-print("I am awake")
+print("Bot is running!")
 
 bot = Client(
     "bot",
@@ -12,5 +21,4 @@ bot = Client(
     bot_token=Config.BOT_TOKEN,
     plugins=plugins
 )
-
 bot.run()
