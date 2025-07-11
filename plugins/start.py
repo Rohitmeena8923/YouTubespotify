016df83d1f233da69d2db766cfbc254c145c5744
Rohitmeena8923 <157653731+Rohitmeena8923@users.noreply.github.com>
2025-07-11 20:38:18 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -2,22 +2,16 @@ from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
 START_BUTTONS = InlineKeyboardMarkup(
-    [
-        [
-            InlineKeyboardButton("🔍Search YouTube", switch_inline_query_current_chat="")
-        ]
-    ]
+    [[InlineKeyboardButton("🔍 Search YouTube", switch_inline_query_current_chat="")]]
 )
 
 @Client.on_message(filters.command("start") & filters.private)
 async def start(bot, message):
-    reply_markup = START_BUTTONS
     await message.reply(
-        f"Hello {message.from_user.first_name}!\n\n"
-        "This bot can search for YouTube videos & download YouTube videos, playlists and more. "
-        "To search videos click the below \"🔍Search Youtube\" button.\n\n"
-        "● /playlist_aud - <i>link</i> :- To download playlist videos in audio format\n"
-        "● /help :- For more info.",
-        reply_markup=reply_markup,
-        parse_mode="HTML"
+        f"Hello {message.from_user.first_name}! 👋\n\n"
+        "This bot can search and download YouTube videos and playlists.\n"
+        "Use /help to learn how to use the bot.",
+        reply_markup=START_BUTTONS,
+        parse_mode="html"  # ✅ lowercase fixed
     )
+)
